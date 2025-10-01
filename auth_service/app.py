19f8c99d5ddbd44d3dc58b58fca18dcca340d44e
Haseeb467 <trickyboy467@gmail.com>
2025-10-01 19:14:21 +0500

    Added new API route for boat

--- auth_service/app.py
+++ auth_service/app.py
@@ -218,7 +218,7 @@ app = FastAPI(title="FastAPI Auth Service")
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://haseeb467.pythonanywhere.com" , "http://localhost:3000" , "http://localhost:5173/" ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
